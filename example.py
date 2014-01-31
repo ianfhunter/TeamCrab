bf21a4d6bbe9dd29c40bebbff046ba5b3b4d5b80
Ian Hunter <hunterif@tcd.ie>
2014-01-31 14:19:49 +0000

    basic example pygame file

---
+++ example.py
@@ -0,0 +1,40 @@
+#Taken from http://inventwithpython.com/
+
+import pygame, sys
+from pygame.locals import *
+
+# set up pygame
+pygame.init()
+
+# set up the window
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Crab SWENG Simulator')	#Title of screen
+
+# set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# set up fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+# set up the text
+text = basicFont.render('Hello world!', True, BLACK)
+
+# draw the white background onto the surface
+windowSurface.fill(WHITE)
+
+# draw the text onto the surface
+windowSurface.blit(text, (200,180))
+
+# draw the window onto the screen
+pygame.display.update()
+
+# run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
