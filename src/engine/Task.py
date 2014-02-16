6402507c197a1da52bc00d777553b62fd34075b0
Hugh Lardner <lardnerl@tcd.ie>
2014-02-16 15:12:04 +0000

    corrected spelling, note on how engine works

--- src/engine/Task.py
+++ src/engine/Task.py
@@ -5,8 +5,8 @@ class Task(object):
     def __init__(self, name, cost, module):
         self.name = name
         self.cost = cost
-        self.progress = 0
-        self.expected_progress = 0
+        self.progress = 0.0
+        self.expected_progress = 0.0
         self.completed = False
         self.problems = list()
         self.module = module
--- src/engine/Team.py
+++ src/engine/Team.py
@@ -2,12 +2,26 @@ import random
 
 class Team(object):
 
-    def __init__(self, name, effiency, salary, size):
+    def __init__(self, name, efficiency, salary, size):
         self.name = name
         self.salary = salary
-        self.efficiency = effiency
+        self.efficiency = efficiency
         self.size = size
         self.task = None
+        
+     """ 
+     expected_progress is 1 point per hour per person on the team - so each hour + size of team.
+
+    Actual progress (Left name as just progress) - 
+     Effected by -
+        efficiency of team - 1 is standard - no modifier - less then 1 is a poor team, greater then 1 is a good team
+        Cultural modifier - 1 is standard - no modifier  - less then 1 is a poor culture , greater then 1 is a good culture
+        Size of team - basic advancement per hour
+        random element - + or - 0 to 25% of the progress made in that hour
+
+    formula - team efficiency * cultural efficiency * size of team + random element
+    random element = +/-  up to 25% of (team efficiency * cultural efficiency * size of team)
+    """
 
     def calc_progress(self, mod):
         if not self.task.completed:
