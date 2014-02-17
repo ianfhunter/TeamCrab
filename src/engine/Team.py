de56f5899fe03ba43643b35ed1dc51d5dafcc94a
Scott Cunningham <cunninsc@tcd.ie>
2014-02-17 23:27:18 +0000

    Removed invisible whitespace in Team.py

--- src/engine/Team.py
+++ src/engine/Team.py
@@ -1,15 +1,14 @@
 import random
 
 class Team(object):
-
     def __init__(self, name, efficiency, salary, size):
         self.name = name
         self.salary = salary
         self.efficiency = efficiency
         self.size = size
         self.task = None
-        
-    """ 
+
+    """
     expected_progress is 1 point per hour per person on the team - so each hour + size of team.
 
     Actual progress (Left name as just progress) - 
@@ -22,13 +21,12 @@ class Team(object):
     formula - team efficiency * cultural efficiency * size of team + random element
     random element = +/-  up to 25% of (team efficiency * cultural efficiency * size of team)
     """
-
     def calc_progress(self, mod):
         if not self.task.completed:
-		
+
             prog = self.efficiency * mod * self.size 
             self.task.progress += prog + self.random_element(prog)
-			
+
             if self.task.expected_progress < self.task.cost:
                 self.task.expected_progress += self.size 
             if self.task.progress >= self.task.cost:
@@ -36,9 +34,9 @@ class Team(object):
                 self.task.module.completed_tasks.append(self.task)
                 self.task.module.tasks.remove(self.task)
                 self.task.completed = True
-				
+
     def random_element(self, prog):
         amount = random.randint(0, 25)
         direction = random.choice([-1, 1])
         return direction * prog / 100 * amount
-        
+
