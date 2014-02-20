11672bcccf0d52c7d850a5da136e8ec0a08961fa
Ian Hunter <hunterif@tcd.ie>
2014-02-20 16:19:37 +0000

    tabs to spaces, ui centering

--- src/UI/endgame.py
+++ src/UI/endgame.py
@@ -7,8 +7,8 @@ class EndGame:
         self.config = config
         self.project = project
         self.screen = screen
-	self.app = gui.App()
-	self.app.connect(gui.QUIT, self.app.quit, None)
+        self.app = gui.App()
+        self.app.connect(gui.QUIT, self.app.quit, None)
         self.contain = gui.Container(width=400,
                                      height=400)
 
@@ -24,15 +24,15 @@ class EndGame:
         return report
 
     def total_person_hours(self):
-	total_estimated = 0
-	total_actual = 0
-	for location in self.project.locations:
-	    for team in location.teams:
-	        for task in team.completed_tasks:
-			estimated_hours = task.cost / team.size
-			total_estimated += estimated_hours	
-			total_actual += task.hours_taken
-	return (total_estimated, total_actual)
+        total_estimated = 0
+        total_actual = 0
+        for location in self.project.locations:
+            for team in location.teams:
+                for task in team.completed_tasks:
+                    estimated_hours = task.cost / team.size
+                    total_estimated += estimated_hours    
+                    total_actual += task.hours_taken
+        return (total_estimated, total_actual)
 
     def refresh_screen(self):
         pygame.display.flip()
@@ -45,51 +45,51 @@ class EndGame:
 
     def draw_endgame(self):
         ''' Shows the user the end game stats and generates a report.'''
-	report = self.generate_report()
+        report = self.generate_report()
         self.write_endgame_csv(report)
 
-	font = pygame.font.SysFont("Helvetica", 15)
-	monofont = pygame.font.SysFont("monospace", 15)
+        font = pygame.font.SysFont("Helvetica", 15)
+        monofont = pygame.font.SysFont("monospace", 15)
         font_large = pygame.font.SysFont("Helvetica", 56)
 
         label = font_large.render("Game Over.", 1, (0, 0, 0))
-        self.screen.blit(label, (250, 20))
+        self.screen.blit(label, (260, 20))
 
-	time = self.project.current_time - self.project.start_time
+        time = self.project.current_time - self.project.start_time
         label = font.render("You finished the project in: " + str(time) + " hours", 1, (0, 0, 0))
         self.screen.blit(label, (80, 100))
 
-	# TODO: Get real cost value here when implemented, currently dummy.
-	cost = 1337
+        # TODO: Get real cost value here when implemented, currently dummy.
+        cost = 1337
         label = font.render("Profit Margin: $" + str(cost), 1, (0, 0, 0))
-        self.screen.blit(label, (80, 150))
+        self.screen.blit(label, (80, 140))
 
-		
-	estimated_hours, actual_hours = self.total_person_hours()
+        
+        estimated_hours, actual_hours = self.total_person_hours()
         label = font.render("Total man hours used:" + str(actual_hours) +
-		" (estimate " + str(estimated_hours) + ")", 1, (0, 0, 0))
-        self.screen.blit(label, (80, 200))
+        " (estimate " + str(estimated_hours) + ")", 1, (0, 0, 0))
+        self.screen.blit(label, (80, 180))
 
 
-	my_list = gui.List(width=750, height=180)
-	
-	table = gui.Table(font=monofont)
+        my_list = gui.List(width=750, height=180)
+        
+        table = gui.Table(font=monofont)
 
-	for (team, module, task, estimate, actual) in report:
-	    # I'm not proud of this
-	    s = team + (" " * (20 - len(team))) + task + (" " * (20 - len(task))) \
-		+ str(estimate) + (" " * (20 - len(str(estimate)))) + str(actual)
-	    
+        for (team, module, task, estimate, actual) in report:
+            # I'm not proud of this
+            s = team + (" " * (20 - len(team))) + task + (" " * (20 - len(task))) \
+            + str(estimate) + (" " * (20 - len(str(estimate)))) + str(actual)
+            
             l = gui.Label(s)
-	    l.set_font(monofont)
-	    my_list.add(l)
+            l.set_font(monofont)
+            my_list.add(l)
 
-	# We need to empty the container's widgets before adding updated ones or else
-	# pgu will draw the new ones over the old ones.
-	self.contain.widgets = []	
-	self.contain.add(my_list, 0, 180)
-    	self.app.init(self.contain)
-    	self.app.paint(self.screen)
+        # We need to empty the container's widgets before adding updated ones or else
+        # pgu will draw the new ones over the old ones.
+        self.contain.widgets = []    
+        self.contain.add(my_list, 0, 180)
+        self.app.init(self.contain)
+        self.app.paint(self.screen)
 
     def draw(self):
         ''' The parent draw function of the end game screen .'''
