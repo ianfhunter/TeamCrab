98a75e2d232144984ecfa5f03999b70da16971d9
lardnerl <lardnerl@lardnerl.(none)>
2014-03-10 16:59:09 +0000

    fix to tests following removal of effiency

--- test/engine/test_team.py
+++ test/engine/test_team.py
@@ -10,16 +10,16 @@ class TestTask(unittest.TestCase):
         pass
 
     def test_constructor(self):
-        team = Team('test_team', 30, 25, 10)
-        self.assertTrue(team.name == 'test_team' and team.efficiency == 30 and team.salary == 25 and team.size == 10)
+        team = Team('test_team', 25, 10)
+        self.assertTrue(team.name == 'test_team' and team.salary == 25 and team.size == 10)
 
     def test_calc_progress(self):
-        team = Team('test_team', 30, 25, 10)
+        team = Team('test_team', 25, 10)
         module = Module('test_module', 800)
         team.module = module
 
         while team.module and not team.module.completed:
-            team.calc_progress(1)
+            team.calc_progress()
 
         self.assertTrue(team.completed_modules and team.completed_modules[0] == module)
 
