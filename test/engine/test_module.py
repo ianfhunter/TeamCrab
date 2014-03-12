bf8332efc923c46495c39b71590ec3809a9df83b
Stephen Rogers <rogers4815@gmail.com>
2014-03-12 21:13:01 +0000

    Fixed a bug introduced into one of the module tests from a recent change

--- test/engine/test_module.py
+++ test/engine/test_module.py
@@ -68,9 +68,10 @@ class TestModule(unittest.TestCase):
     def test_calc_deadline(self):
         module = Module('test_module', 600)
         start_time = datetime.datetime(2014,1,1,0,0,0)
-        deadline = datetime.datetime(2014,1,8,4,0,0)
+        deadline = datetime.datetime(2014,1,16,0,0,0)
 
         module.calc_deadline(start_time, 10)
+        print module.deadline
         self.assertTrue(module.deadline == deadline)
 
         module.calc_deadline(start_time, 600)
